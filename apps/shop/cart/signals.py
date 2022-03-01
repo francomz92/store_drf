dee94535fec328712d99935b5053ab367bc0cf78
Franco Muñoz <franco.mz92@hotmail.com>
2022-03-01 15:53:03 -0300

    disable threads on signals

--- apps/shop/cart/signals.py
+++ apps/shop/cart/signals.py
@@ -1,15 +1,20 @@
+import threading
+
 from django.dispatch import receiver
 from django.db.models.signals import post_save, post_delete
 
-from .models import Cart, CartItem
-from apps.shop.products.models import Product
+from .models import CartItem
 
 
 @receiver(signal=post_save, sender=CartItem)
 def cart_item_post_save(sender, instance, created, **kwargs):
+   # thread = threading.Thread(target=instance.cart.save, daemon=True)
+   # thread.start()
    instance.cart.save()
 
 
 @receiver(signal=post_delete, sender=CartItem)
 def cart_item_post_delete(sender, instance, **kwargs):
+   # thread = threading.Thread(target=instance.cart.save, daemon=True)
+   # thread.start()
    instance.cart.save()
--- apps/users/signals.py
+++ apps/users/signals.py
@@ -1,3 +1,5 @@
+import threading
+
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 from django.contrib.auth import get_user_model
