fa5321b4feb5bd68595af1c5ea93f8fbdef9837c
Franco Muñoz <franco.mz92@hotmail.com>
2022-03-15 15:38:06 -0300

    create Card component

---
+++ client/components/Card.js
@@ -0,0 +1,24 @@
+export const card = (data) => {
+   const $card = document.createElement('article');
+   $card.setAttribute('id', data.id);
+   $card.innerHTML = `
+      <img src="${data.image_url}" alt="${data.name}" />
+      <h5>${data.name}</h5>
+      `;
+   if (data.offer) {
+      $card.innerHTML += `
+         <span class="on-sale">On Sale</span>
+         <span class="discount-rate">${data.discount_rate}%</span>
+         <span class="unit-price-on-discount">$${data.unit_price}</span>
+         <span class="price-discount">$${data.price_with_discount}</span>
+         `;
+   } else {
+      $card.innerHTML += `
+         <span class="unit-price">$${data.unit_price}</span>
+         `;
+   }
+   $card.innerHTML += `
+      <span>Quedan ${data.stok}</span>
+   `;
+   return $card;
+};
