8e0e37c7f0a2eb6d41c7bf23eb75b214a8f186e6
Franco Muñoz <franco.mz92@hotmail.com>
2022-03-20 22:25:48 -0300

    refactor index.html and printProductCards on client

--- client/helpers/printProductCards.js
+++ client/helpers/printProductCards.js
@@ -1,18 +1,12 @@
 import { card } from '../components/Card.js';
 import { getProductsData } from '../apis/getProducts.js';
-
-const setLinkStyles = () => {
-   const $link = document.createElement('link');
-   $link.setAttribute('href', './assets/styles/card.css');
-   $link.setAttribute('rel', 'stylesheet');
-   document.head.appendChild($link);
-};
+import { setLinkStyles } from './setLinkStyle.js';
 
 export const prinProductCards = async () => {
    const $grid = document.getElementById('card-grid');
    const data = await getProductsData();
    if (data.results.length > 0) {
-      setLinkStyles();
+      setLinkStyles('./assets/styles/card.css');
       data.results.forEach((product) => {
          $grid.appendChild(card(product));
       });
