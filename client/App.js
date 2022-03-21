7803f7df76a54bec2964d7ef861584036a652815
Franco Muñoz <franco.mz92@hotmail.com>
2022-03-20 22:35:09 -0300

    implement printHeader and printStoreAsideContent in Apps.js

--- client/App.js
+++ client/App.js
@@ -1,5 +1,13 @@
 import { prinProductCards } from './helpers/printProductCards.js';
+import { printHeader } from './helpers/printHeader.js';
+import { printStoreAsideContent } from './helpers/printStoreAsideContent.js';
+
+const user = localStorage.getItem('user');
 
 document.addEventListener('DOMContentLoaded', (e) => {
+   let userData = null;
+   if (user) userData = JSON.parse(user).user;
+   printHeader(userData);
    prinProductCards();
+   printStoreAsideContent();
 });
