1efa7655ccacb3fc19f383f6ea75303fc318ef1f
Franco Muñoz <franco.mz92@hotmail.com>
2022-03-20 22:16:57 -0300

    create public category list endpoint

--- server/apps/shop/categories/urls.py
+++ server/apps/shop/categories/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from apps.shop.categories.views import CategoryListCreateView, CategoryRetrieveUpdateDestroyView
+from apps.shop.categories.views import CategoryListCreateView, CategoryRetrieveUpdateDestroyView, PublicCategoryListView
 
 app_name = 'categories'
 
 urlpatterns = [
-    path('category/', CategoryListCreateView.as_view(), name='categories'),
-    path('category/<int:id>/', CategoryRetrieveUpdateDestroyView.as_view(), name='category_detail'),
+    # Private Endpoints
+    path('private/category/', CategoryListCreateView.as_view(), name='categories'),
+    path('private/category/<int:id>/', CategoryRetrieveUpdateDestroyView.as_view(), name='category_detail'),
+    # Public Endpoints
+    path('public/category/', PublicCategoryListView.as_view(), name='category_list'),
 ]
--- server/apps/shop/categories/views.py
+++ server/apps/shop/categories/views.py
@@ -5,10 +5,20 @@ from rest_framework import generics, filters, permissions
 from . import serializers
 
 
+class PublicCategoryListView(generics.ListAPIView):
+   serializer_class = serializers.CategorySerializer
+   filter_backends = (filters.SearchFilter, )
+   search_fields = ('name', )
+
+   def get_queryset(self):
+      return serializers.models.Category.objects.all().order_by('id')
+
+
 class CategoryListCreateView(generics.ListCreateAPIView):
    serializer_class = serializers.CategorySerializer
    filter_backends = (filters.SearchFilter, )
    search_fields = ('name', )
+
    permission_classes = (permissions.IsAuthenticated, )
 
    def get_queryset(self):
