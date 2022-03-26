668b33c93317e72078027337b8617797dd19d00a
Franco Muñoz <franco.mz92@hotmail.com>
2022-03-26 01:37:10 -0300

    refactor Apps.js with store section handler implementation

--- client/App.js
+++ client/App.js
@@ -1,11 +1,11 @@
 import { printHeader } from './helpers/printHeader.js';
-import { printStoreSection } from './helpers/printStoreSection.js';
+import { storeSectionHandler } from './handlers/storeSectionHandler.js';
 
 const user = localStorage.getItem('user');
 
-document.addEventListener('DOMContentLoaded', (e) => {
+document.addEventListener('DOMContentLoaded', async (e) => {
    let userData = null;
-   if (user) userData = JSON.parse(user).user;
-   printHeader(userData);
-   printStoreSection();
+   if (user) userData = JSON.parse(user);
+   printHeader(userData.user);
+   storeSectionHandler(userData);
 });
