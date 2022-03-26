0a8f04be62ae917a4dd0ef92b522bfbd8ded2076
Franco Muñoz <franco.mz92@hotmail.com>
2022-03-26 20:35:56 -0300

    implement query filters for product get api

--- client/apis/getProducts.js
+++ client/apis/getProducts.js
@@ -1,6 +1,30 @@
-export const getProductsData = async () => {
+/* 
+   queryFilters valid
+   {
+      category__name: 'String',
+      name: 'String',
+      unit_price: Number,
+      offer: 'Boolean',
+   };
+*/
+
+const getFilters = (filters) => {
+   let setedFilters = '';
+   for (const key in filters) {
+      if (Object.hasOwnProperty.call(filters, key) && Boolean(filters[key]) !== false) {
+         setedFilters += `${key}=${filters[key]}&`;
+      }
+   }
+   if (setedFilters.endsWith('&')) {
+      setedFilters = setedFilters.slice(0, setedFilters.length - 1);
+   }
+   return setedFilters;
+};
+
+export const getProductsData = async (queryFilters) => {
    try {
-      const response = await fetch('http://localhost:8000/api/shop/products/public/');
+      const filters = getFilters(queryFilters);
+      const response = await fetch(`http://localhost:8000/api/shop/products/public/?${filters}`);
       const products = await response.json();
       if (!response.ok) throw response;
       return products;
