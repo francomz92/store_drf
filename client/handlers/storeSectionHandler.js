ba8f22ffe2d7731de1ec31852b4b5f98816dc2d0
Franco Muñoz <franco.mz92@hotmail.com>
2022-04-02 02:34:24 -0300

    implement filter container adding offer filter

--- client/handlers/storeSectionHandler.js
+++ client/handlers/storeSectionHandler.js
@@ -4,11 +4,17 @@ import { printStoreAsideContent } from '../helpers/printStoreAsideContent.js';
 import { printProductCards } from '../helpers/printProductCards.js';
 import { setCartItem } from '../apis/setCartItem.js';
 
+const filters = {
+   category__name: null,
+   name: null,
+   unit_price: null,
+   offer: null,
+};
+
 export const storeSectionHandler = async (userData) => {
    setLinkStyles('../assets/styles/section.store.css');
 
    const $aside = await printStoreAsideContent();
-   // const $selectFilter = $aside.querySelector('select[name=category]');
    const productsResponse = await getProductsData();
    const { $grid, data } = printProductCards(productsResponse);
 
@@ -28,10 +34,19 @@ export const storeSectionHandler = async (userData) => {
 
    $aside.addEventListener('change', async (e) => {
       e.preventDefault();
-      if (e.target.matches('select[name=category]')) {
-         const productsResponse = await getProductsData({ category__name: e.target.value });
-         document.getElementById('card-grid').innerHTML = '';
-         printProductCards(productsResponse);
+      if (e.target.matches('select[name=category]')) filters['category__name'] = e.target.value;
+      if (e.target.matches('input[name=offer]')) {
+         if (e.target.getAttribute('checked') !== 'true') {
+            filters['offer'] = true;
+            e.target.setAttribute('checked', true);
+         } else {
+            filters['offer'] = null;
+            e.target.setAttribute('checked', false);
+         }
       }
+
+      const productsResponse = await getProductsData(filters);
+      document.getElementById('card-grid').innerHTML = '';
+      printProductCards(productsResponse);
    });
 };
