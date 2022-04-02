d4316257e7530007395f2e95c5d6059df8a8cfe9
Franco Muñoz <franco.mz92@hotmail.com>
2022-04-02 02:32:53 -0300

    implement filterContainer for select and checkbox container

--- client/helpers/printStoreAsideContent.js
+++ client/helpers/printStoreAsideContent.js
@@ -1,6 +1,8 @@
+import { setLinkStyles } from './setLinkStyle.js';
 import { getCategories } from '../apis/getCategories.js';
 import { selectFilter } from '../components/SelectFilter.js';
-import { setLinkStyles } from './setLinkStyle.js';
+import { checkBoxFilter } from '../components/CheckBoxFilter.js';
+import { filterContainer } from '../components/FilterContainer.js';
 
 export const printStoreAsideContent = async () => {
    const $aside = document.querySelector('.store-aside');
@@ -8,9 +10,15 @@ export const printStoreAsideContent = async () => {
 
    setLinkStyles('../assets/styles/aside.css');
 
-   $aside.innerHTML = `
-      <summary>Filtros</summary>
-   `;
-   $aside.appendChild(selectFilter(categories));
+   $aside.appendChild(
+      filterContainer({
+         labelName: 'Categoría:',
+         labelReference: 'category',
+         filter: selectFilter('category', categories),
+      })
+   );
+   $aside.appendChild(
+      filterContainer({ labelName: 'En oferta: ', labelReference: 'offer', filter: checkBoxFilter('offer') })
+   );
    return $aside;
 };
