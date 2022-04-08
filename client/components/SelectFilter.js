fab00d55f6ffc8249c69ab9c88d1e7c5bc43be46
Franco Muñoz <franco.mz92@hotmail.com>
2022-04-08 16:14:09 -0300

    refactor select filter compenent adding extra args

--- client/components/SelectFilter.js
+++ client/components/SelectFilter.js
@@ -1,15 +1,19 @@
-export const selectFilter = (nameAttr, objArray) => {
+export const selectFilter = ({ nameAttr, objArray, styles = [], placeHolder = '' }) => {
    const $select = document.createElement('select');
 
    $select.setAttribute('name', nameAttr);
+   if (styles.length > 0) {
+      styles.forEach((style) => $select.classList.add(style));
+   }
+
    $select.innerHTML = `
-      <option selected value="">Todos</option>
+      <option selected value="">${placeHolder}</option>
    `;
 
    objArray.forEach((obj) => {
       const $option = document.createElement('option');
       $option.setAttribute('value', obj.name);
-      $option.textContent = obj.name;
+      $option.textContent = obj.alias || obj.name;
       $select.appendChild($option);
    });
 
