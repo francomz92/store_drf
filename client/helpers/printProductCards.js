59650c0a85b89d779ce193e96b1816453703fb71
Franco Muñoz <franco.mz92@hotmail.com>
2022-04-08 16:13:21 -0300

    rename variables in product card helper

--- client/helpers/printProductCards.js
+++ client/helpers/printProductCards.js
@@ -1,14 +1,15 @@
 import { card } from '../components/Card.js';
 import { setLinkStyles } from './setLinkStyle.js';
 
-export const printProductCards = (data) => {
+export const printProductCards = (productData) => {
    const $grid = document.getElementById('card-grid');
-   setLinkStyles('./assets/styles/card.css');
 
-   if (data.results.length > 0) {
-      data.results.forEach((product) => $grid.appendChild(card(product)));
+   setLinkStyles('./assets/styles/card.css');
+   // Set product card
+   if (productData.results.length > 0) {
+      productData.results.forEach((product) => $grid.appendChild(card(product)));
    } else {
       $grid.innerHTML = '<h3 class="card-grid-empty">There are no products to display</h3>';
    }
-   return { $grid, data };
+   return { $grid, productData };
 };
