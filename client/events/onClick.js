e471e19e5b89756475911953c935ff5aa367eeb2
Franco Muñoz <franco.mz92@hotmail.com>
2022-04-14 19:11:12 -0300

    actualize cart item cuantity on product card click event

--- client/events/onClick.js
+++ client/events/onClick.js
@@ -1,15 +1,18 @@
 import { addItemToCart } from '../apis/setCartItem.js';
+import { getCart } from '../apis/getCart.js';
 
-export const initCardClickEvent = ({ nodeListening, ProductData, userData }) => {
+export const initCardClickEvent = ({ nodeListening, ProductData, userData }, cart) => {
    nodeListening.querySelectorAll('article').forEach((article) => {
-      article.addEventListener('click', (e) => {
+      article.addEventListener('click', async (e) => {
          e.preventDefault();
          e.stopPropagation();
          const item = {
             product: ProductData.results.find((element) => +element.id === +article.id),
             ammount: 1,
          };
-         addItemToCart(userData, item);
+         await addItemToCart(userData, item);
+         cart = await getCart(userData);
+         document.querySelector('#cart').textContent = cart.results.items.length;
       });
    });
 };
