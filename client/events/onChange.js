8958a6debaa13db6096280ad4608a79fab600199
Franco Muñoz <franco.mz92@hotmail.com>
2022-04-14 17:04:11 -0300

    implement price range filter in onChange events

--- client/events/onChange.js
+++ client/events/onChange.js
@@ -4,7 +4,8 @@ import { printProductCards } from '../helpers/printProductCards.js';
 const filters = {
    category__name: null,
    name: null,
-   unit_price: null,
+   unit_price__gte: 0,
+   unit_price__lte: null,
    offer: null,
    ordering: null,
 };
@@ -23,6 +24,16 @@ export const initStoreSectionChangeEvent = ({ nodeListening }) => {
          }
       }
       if (e.target.matches('select[name=ordering]')) filters['ordering'] = e.target.value;
+      if (e.target.matches('input[name=unit_price__lte]')) {
+         filters['unit_price__lte'] = e.target.value;
+         e.target.setAttribute('title', e.target.value);
+         e.target.previousElementSibling.textContent = `Hasta: $${e.target.value}`;
+      }
+      if (e.target.matches('input[name=unit_price__gte]')) {
+         filters['unit_price__gte'] = e.target.value;
+         e.target.setAttribute('title', e.target.value);
+         e.target.previousElementSibling.textContent = `Desde: $${e.target.value}`;
+      }
 
       const productsResponse = await getProductsData(filters);
       document.getElementById('card-grid').innerHTML = '';
