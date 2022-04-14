3b6d3ca98cc3d2328106158989f56e7d6bda6fce
Franco Muñoz <franco.mz92@hotmail.com>
2022-04-14 19:09:44 -0300

    rafactor header helper showing cart items quantity

--- client/helpers/printHeader.js
+++ client/helpers/printHeader.js
@@ -1,7 +1,7 @@
 import { header } from '../components/Header.js';
 import { setLinkStyles } from './setLinkStyle.js';
 
-export const printHeader = (userData) => {
+export const printHeader = (userData, cart) => {
    setLinkStyles('../assets/styles/index.header.css');
-   document.body.insertAdjacentElement('afterbegin', header(userData));
+   document.body.insertAdjacentElement('afterbegin', header(userData, cart));
 };
