1a857ea2f83c567d7f2fc7c93d8c27274aea05af
Franco Muñoz <franco.mz92@hotmail.com>
2022-05-07 20:37:31 -0300

    refactor App.js

--- client/App.js
+++ client/App.js
@@ -1,6 +1,7 @@
 import { getCart } from './apis/cart.js';
 import { loadStoreSection } from './helpers/loaders/storeSection.js';
 import { headerHandler } from './handlers/headerHandler.js';
+import { setActiveLink } from './helpers/header.js';
 
 const user = localStorage.getItem('user');
 let userData = null;
@@ -13,23 +14,45 @@ document.addEventListener('DOMContentLoaded', async (e) => {
       cart = await getCart(userData);
    }
 
-   headerHandler(userData, cart);
+   const $header = headerHandler(userData, cart);
 
    // On load specific path url
-   if (location.hash === '#store') {
-      document.head.querySelector('title').textContent = 'Store';
-      loadStoreSection($main, userData, cart);
-   } else if (location.hash === '#contact') {
-      document.head.querySelector('title').textContent = 'Contact';
-   } else document.head.querySelector('title').textContent = 'Home';
+   setActiveLink($header.querySelector('nav'), 'a');
+
+   switch (location.hash) {
+      case '#store':
+         document.head.querySelector('title').textContent = 'Store';
+         loadStoreSection($main, userData, cart);
+         break;
+      case '#contact':
+         document.head.querySelector('title').textContent = 'Contact';
+         break;
+      case '#sign-in':
+         document.head.querySelector('title').textContent = 'Sign In';
+         break;
+      case '#sign-up':
+         document.head.querySelector('title').textContent = 'Sign Up';
+         break;
+      default:
+         document.head.querySelector('title').textContent = 'Home';
+         break;
+   }
 
    // On change path url event
    window.addEventListener('hashchange', (e) => {
+      setActiveLink($header.querySelector('nav'), 'a');
+
       if (e.newURL.match('/#store')) {
          document.head.querySelector('title').textContent = 'Store';
          loadStoreSection($main, userData, cart);
       } else if (e.newURL.match('/#contact')) {
          document.head.querySelector('title').textContent = 'Contact';
+         $main.innerHTML = '';
+      } else if (location.hash === '#sign-in') {
+         document.head.querySelector('title').textContent = 'Sign In';
+         $main.innerHTML = '';
+      } else if (location.hash === '#sign-up') {
+         document.head.querySelector('title').textContent = 'Sign Up';
          $main.innerHTML = '';
       } else {
          document.head.querySelector('title').textContent = 'Home';
