e1699273ec604b5446214b743de653be25664d65
Franco Muñoz <franco.mz92@hotmail.com>
2022-05-08 07:50:14 -0300

    fix null unit price when creating product

--- server/apps/shop/products/models.py
+++ server/apps/shop/products/models.py
@@ -9,23 +9,29 @@ class Product(GenericModel):
    name = models.CharField(verbose_name=_('Name'), max_length=100)
    description = models.TextField(verbose_name=_('Description'), max_length=500)
    unit_price = models.DecimalField(verbose_name=_('Unit Price'),
+                                    default=0,
                                     max_digits=8,
                                     decimal_places=2,
                                     validators=[validators.MinValueValidator(0.01)])
    image_url = models.ImageField(verbose_name=_('Image'), upload_to='products/images/', null=True, blank=True)
    offer = models.BooleanField(verbose_name=_('On sale'), default=False)
-   discount_rate = models.PositiveSmallIntegerField(verbose_name=_('Discount Rate'),
-                                                    default=0,
-                                                    validators=[
-                                                        validators.MinValueValidator(1),
-                                                        validators.MaxValueValidator(100),
-                                                    ])
+   discount_rate = models.IntegerField(verbose_name=_('Discount Rate'),
+                                       default=0,
+                                       null=False,
+                                       blank=False,
+                                       validators=[
+                                           validators.MinValueValidator(0),
+                                           validators.MaxValueValidator(100),
+                                       ])
    price_with_discount = models.DecimalField(verbose_name=_('Price with discount'),
                                              max_digits=8,
                                              decimal_places=2,
                                              default=0,
                                              editable=False)
-   stok = models.PositiveIntegerField(verbose_name=_('Stock'), validators=[validators.MinValueValidator(0)])
+   stok = models.PositiveIntegerField(verbose_name=_('Stock'),
+                                      default=0,
+                                      null=False,
+                                      validators=[validators.MinValueValidator(0)])
    active = models.BooleanField(verbose_name=_('Active'), default=True)
 
    class Meta:
@@ -42,7 +48,9 @@ class Product(GenericModel):
          self.price_with_discount = self.unit_price - (self.unit_price * self.discount_rate / 100)
 
    def _check_active(self):
-      if self.stok <= 1:
+      if not self.stok:
+         self.active = False
+      elif self.stok <= 1:
          self.active = False
 
    def clean(self) -> None:
